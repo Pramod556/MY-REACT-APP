43723635b247c3481d81dfa5356e3f45899d42cf
Pramod Swargam <98465143+Pramod556@users.noreply.github.com>
2023-08-05 20:41:42 +0530

    added all files

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return ( 
+        <footer>
+<div class="footer">
+<div class="row">
+<a href=""><i class="fa fa-linkedin"></i></a>
+<a href=""><i class="fa fa-instagram"></i></a>
+<a href=""><i class="fa fa-github"></i></a>
+<a href=""><i class="fa fa-"></i></a>
+</div>
+
+
+<div class="row">
+  Copyright © 2023 - All rights reserved || Designed By: Pramod
+</div>
+</div>
+</footer>
+
+     );
+}
+ 
+export default Footer;
